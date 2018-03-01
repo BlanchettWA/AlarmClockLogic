c372db409e5a4035e52e181f79e0d8c5fc244681
William Blanchett <blanchettwa@g.cofc.edu>
2018-03-01 09:38:50 -0500

    Began Project on NetBeans

---
+++ daLAWAlarmClock/src/com/csci360/alarmclock/Alarm.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.csci360.alarmclock;
+
+/**
+ *
+ * @author Liam
+ */
+public class Alarm {
+    
+    private Time triggerTime;
+    private Time snoozeTime;
+    
+    //If true, then use a built in sound and not the radio
+    private boolean beepSound;
+    
+    private boolean enableState;
+    
+    private boolean snoozeState;
+    
+    
+    
+}
---
+++ daLAWAlarmClock/src/com/csci360/alarmclock/AlarmClock.java
@@ -0,0 +1,21 @@
+/*
+Beginning clas file for Alarm Clock. 
+
+This is a controller for the rest of the application
+ */
+package com.csci360.alarmclock;
+
+import java.util.Timer;
+
+
+public class AlarmClock {
+   private Timer runner;
+   
+   private Time sysTime;
+   
+   private Alarm alarm1;
+   private Alarm alarm2;
+   
+   private Radio radio;
+    
+}

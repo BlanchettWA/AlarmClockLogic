d684d0edaefb06fde7f5ef45afbc3a53c75ee53b
lukemanzo <35337237+lukemanzo@users.noreply.github.com>
2018-03-05 13:32:35 -0500

    Update Time.java

--- daLAWAlarmClock/src/com/csci360/alarmclock/Time.java
+++ daLAWAlarmClock/src/com/csci360/alarmclock/Time.java
@@ -3,12 +3,15 @@ This is a class for the simulation of time. It stores in military time.
 It also returns military time. 
  */
 package com.csci360.alarmclock;
-
+import java.util.Date; 
+import java.util.Timer; 
+import java.util.TimerTask;
 
 public class Time {
     
     private int hour;
     private int minute;
+    private static int seconds = 0; 
 
 public int getHour()
 {
@@ -39,5 +42,22 @@ public int[] getTime()
     
     return sTime;
 }
+    
+ public static void main(String[] args) {
+        
+    Timer timer = new Timer();
+    timer.scheduleAtFixedRate(new TimerTask() {
+        public void run() {
+            seconds++; 	//1 second = 1000 elapsed ms
+            System.out.println(seconds);  //display time in seconds
+            if (seconds == 20) {	  //example of stopping the timer at desired time
+                timer.cancel();   //cancels the running timer, could be implemented for alarm use
+
+                }
+            }
+        }, new Date(), 1000);  //period b/w incrementations = 1000 ms
+        //new Date() is necessary for scheduleAtFixedRate Timer/TimerTask function
+
+    }
 
 }

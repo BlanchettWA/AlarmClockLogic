5fd5412466c3a0ccf6fd49a93e769fa5c55c0398
ajsupreme <williamsaj2@g.cofc.edu>
2018-03-06 12:38:48 -0500

    OOh

--- daLAWAlarmClock/test/com/csci360/alarmclock/AlarmTest.java
+++ daLAWAlarmClock/test/com/csci360/alarmclock/AlarmTest.java
@@ -42,5 +42,84 @@ public class AlarmTest {
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
+
+    /**
+     * Test of specifyTime method, of class Alarm.
+     */
+    @Test
+    public void testSpecifyTime() {
+        System.out.println("specifyTime");
+        int h = 0;
+        int m = 0;
+        Alarm instance = new Alarm();
+        instance.specifyTime(h, m);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of specifySound method, of class Alarm.
+     */
+    @Test
+    public void testSpecifySound() {
+        System.out.println("specifySound");
+        boolean snd = false;
+        Alarm instance = new Alarm();
+        instance.specifySound(snd);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of enableAlarm method, of class Alarm.
+     */
+    @Test
+    public void testEnableAlarm() {
+        System.out.println("enableAlarm");
+        Alarm instance = new Alarm();
+        instance.enableAlarm();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of disableAlarm method, of class Alarm.
+     */
+    @Test
+    public void testDisableAlarm() {
+        System.out.println("disableAlarm");
+        Alarm instance = new Alarm();
+        instance.disableAlarm();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getTriggerTime method, of class Alarm.
+     */
+    @Test
+    public void testGetTriggerTime() {
+        System.out.println("getTriggerTime");
+        Alarm instance = new Alarm();
+        Time expResult = null;
+        Time result = instance.getTriggerTime();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getStatus method, of class Alarm.
+     */
+    @Test
+    public void testGetStatus() {
+        System.out.println("getStatus");
+        Alarm instance = new Alarm();
+        boolean expResult = false;
+        boolean result = instance.getStatus();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
     
 }
